79ead6045e5282bbcb9c6ba54fcfbb72a2cf8ab9
Alexandre Santos <alexandre.santana2201@gmail.com>
2025-09-30 13:59:37 -0300

    Projeto Criado

---
+++ GerenciadorLivro/src/main/java/com/example/GerenciadorLivro/GerenciadorLivroApplication.java
@@ -0,0 +1,13 @@
+package com.example.GerenciadorLivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorLivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorLivroApplication.class, args);
+	}
+
+}
---
+++ GerenciadorLivro/src/test/java/com/example/GerenciadorLivro/GerenciadorLivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GerenciadorLivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorLivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
